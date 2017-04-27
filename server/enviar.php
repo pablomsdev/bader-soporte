716d7c40afeb65429f21db29ee167d0dcd84135f
Pablo Méndez Solla <pablomsdev@gmail.com>
2017-04-27 18:53:30 +0200

    Envio de correo con datos de formulario

--- server/enviar.php
+++ server/enviar.php
@@ -3,7 +3,31 @@
 
 //Definición de variables
 $nombre_archivo = 'texto.txt';
-$contenido = "\r\nNombre:" . $_POST["first_name"] ."\r\nApellidos:" . $_POST["last_name"] ."\r\n\r\n\r\n";
+$contenido = "\r\nNombre:" . $_POST["first_name"] ."\r\nApellidos:" . $_POST["last_name"] .
+"\r\nEmail:" . $_POST["email"] ."\r\nTelefono:" . $_POST["phone"] ."\r\nProducto:" .
+ $_POST["product"] ."\r\nProveedor:" . $_POST["dealer"] ."\r\nFecha:" .
+  $_POST["fechacompra"] ."\r\nComentarios:" . $_POST["comment"] ."\r\n\r\n\r\n";
+
+if(isset($_POST['submit'])){
+    $to = "pabloinf@gmail.com"; // this is your Email address
+    $from = $_POST['email']; // this is the sender's Email address
+    $first_name = $_POST['first_name'];
+    $last_name = $_POST['last_name'];
+    $subject = "BADER · Formulario de Soporte";
+    $subject2 = "BADER · Copia de tu solicitud de soporte";
+    $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $contenido;
+    $message2 = "Esta es una copia de tu mensaje " . $first_name . "\n\n" . $contenido . "\n\nContactaremos con usted en la mayor brevedad";
+
+    $headers = "From:" . $from;
+    $headers2 = "From:" . $to;
+    mail($to,$subject,$message,$headers);
+    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+    echo "Correo enviado. Gracias " . $first_name . ", Contactaremos con usted en la mayor brevedad.";
+
+    // You can also use header('Location: thank_you.php'); to redirect to another page.
+    }
+
+//Creamos un log de toda la gente que cubre el formulario
 
 //Se comprueba si el fichero se puede escribir
 if (is_writable($nombre_archivo)) {
