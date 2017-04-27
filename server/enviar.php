c36cbd04dddf2060b323bffff9f91e7e81f19929
Pablo Méndez Solla <pablomsdev@gmail.com>
2017-04-27 17:46:51 +0200

    Pruebas con generacion de fichero

---
+++ server/enviar.php
@@ -0,0 +1,28 @@
+<?php
+  session_start();
+
+//Definición de variables
+$nombre_archivo = 'texto.txt';
+$contenido = "\r\nNum. Albaran:" . $_POST["first_name"] ."\r\nApellidos:" . $_POST["last_name"] ."\r\n\r\n\r\n";
+
+//Se comprueba si el fichero se puede escribir
+if (is_writable($nombre_archivo)) {
+	//Comprobaciones de error
+    if (!$gestor = fopen($nombre_archivo, 'a')) {
+         echo "Error 2 - No se puede abrir el archivo para guardar su texto. Por favor, si el problema persiste contacte con el administrador.</p>";
+         exit;
+    }
+    if (fwrite($gestor, $contenido) === FALSE) {
+        echo "Error 3 - No se puede escribir al archivo para guardar su texto. Por favor, si el problema persiste contacte con el administrador.</p>";
+        exit;
+    }
+    echo "En breve nos pondremos en contacto con usted.</p>";
+	$href="http://www.bader.es";
+	$nombr="BADER";
+	echo "<a href='".$href."'>".$nombr."</a>";
+	sleep(3);
+	header('Location: http://217.160.128.108/facturas-expodental/gracias.html');
+    fclose($gestor);
+} else {
+    echo "Error 1 - No se puede escribir sobre el archivo para guardar su texto. Por favor, si el problema persiste contacte con el administrador.</p>";
+}
